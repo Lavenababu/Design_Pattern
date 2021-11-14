91951eef785864435f1ef2026ab886efafb5c216
Lavena Babu <89774994+Lavenababu@users.noreply.github.com>
2021-11-14 13:26:07 +0530

    Add files via upload

---
+++ Facade/FacadeMain.java
@@ -0,0 +1,16 @@
+package Facade;
+
+public class FacadeMain {
+
+    public static void main(String[] args) {
+
+        CPU cpu = new CPU();
+        Memory memory = new Memory();
+        HardDrive hd = new HardDrive();
+
+
+        ComputerFacade computerFacade = new ComputerFacade(cpu, memory, hd);
+        computerFacade.start();
+
+    }
+}
